cb2cd35b0568ae5811ac0aef73975e9766a7a7b1
Orlin-Rangel <orangellambrano@correo.unicordoba.edu.co>
2023-03-15 19:59:15 -0500

    Movimiento del personaje

--- src/player.js
+++ src/player.js
@@ -2,83 +2,48 @@ export class Player extends Phaser.Scene {
     constructor() {
         super({ key: "Player" });
 
-        this.selected = false;
-        this.startPos = { x: 0, y: 0 };
+        this.velocity = 200; 
     }
 
     preload() {
-        this.load.image("personaje", "./Personaje.png");
-        this.cameras.main.setBackgroundColor("rgb(100, 20, 02, 0.1)");
-
+        this.load.image("personaje", "./Personaje.png"); 
     }
 
     create() {
+        this.scene.launch('Mapa');
+        this.scene.bringToTop('Player'); 
 
-        this.scene.launch('Mapa'); // Se agrega la escena Player
-        this.scene.bringToTop('Player'); // Se trae la escena Escena1 al frente
-        
         this.personaje = this.add
             .sprite(200, 200, "personaje")
             .setScale(0.3, 0.3)
             .setInteractive();
+    }
 
-        // Seleccionar al personaje
-        this.personaje.on("pointerdown", () => {
-            this.selected = true;
-            this.startPos.x = this.personaje.x;
-            this.startPos.y = this.personaje.y;
-            this.personaje.setTint(0x00ff00);
-        });
+    update(time, delta) {
 
-        // Mover y rotar al personaje
-        this.input.on("pointerdown", (pointer) => {
-            if (this.selected) {
-                const velocidad = 200; // 100 px/s
-                const dx = pointer.x - this.personaje.x;
-                const dy = pointer.y - this.personaje.y;
-                const distancia = Math.sqrt(dx * dx + dy * dy);
-                const duracion = distancia / velocidad * 1000;
-                console.log(duracion);
+        //MOVIMIENTO DEL PERSONAJE. 
+        let dx = 0, dy = 0;
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown) {
+            dx -= 1;
+        }
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D).isDown) {
+            dx += 1;
+        }
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) {
+            dy -= 1;
+        }
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S).isDown) {
+            dy += 1;
+        }
 
-                if (distancia > 50 && duracion) {
-                    const angle = Phaser.Math.Angle.BetweenPoints(this.personaje, pointer) * 180 / Math.PI;
-                    const rotateTween = this.tweens.add({
-                        targets: this.personaje,
-                        angle: angle,
-                        ease: "Linear",
-                        duration: distancia / velocidad * 80,
-                    });
-
-                    const moveTween = this.tweens.add({
-                        targets: this.personaje,
-                        x: pointer.x,
-                        y: pointer.y,
-                        ease: "Linear",
-                        duration: duracion,
-                        onComplete: () => {
-                            this.selected = false;
-                            this.personaje.setTint(0xffffff);
-                            this.input.setDefaultCursor("default");
-                        },
-                    });
-
-                    // Transición a la posición original después de la rotación
-                    rotateTween.onComplete = () => {
-                        this.tweens.add({
-                            targets: this.personaje,
-                            x: pointer.x,
-                            y: pointer.y,
-                            ease: "Linear",
-                            duration: duracion,
-                            onComplete: () => {
-                                this.selected = false;
-                                this.personaje.setTint(0xffffff);
-                                this.input.setDefaultCursor("default");
-                            },
-                        });
-                    }
-                }
-            }
-        });
+        if (dx !== 0 || dy !== 0) {
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            const normalizedX = dx / distance;
+            const normalizedY = dy / distance;
+            this.personaje.x += normalizedX * this.velocity * delta  / 1000;
+            this.personaje.y += normalizedY * this.velocity * delta / 1000;
+            const angle = Phaser.Math.Angle.Between(0, 0, normalizedX, normalizedY);
+            this.personaje.angle = angle * 180 / Math.PI;
+        }
     }
 }
